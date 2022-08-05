97832e6489cfec2c2103f18101d0213a0789d55a
Navi-Beep <estawars30@gmail.com>
2022-08-05 14:04:50 -0500

    planning out details

---
+++ rental_calc.py
@@ -0,0 +1,55 @@
+import math
+
+
+go = True
+
+class ROI:
+    pass
+
+    def __init__(self):
+
+
+
+    #define a function for income
+        #ask input for rental income
+        #ask input for landry income
+        #ask input for storage income
+        #ask input for misc income
+    #total all of the income for total monthly income
+
+    #define a function for expenses
+     #ask for input for: taxes
+     #ask for input for: insurance
+     #ask for input for: water and sewer
+     #ask for input for garbage
+     #ask for input for electric/gas
+     #ask for input for: HOA fees
+     #ask for input for: lawn/snow
+     #ask for input for: vacancy
+     #ask for input for repairs
+     #ask for input for capex
+     #ask for input for: prop managment
+     #ask for input for: mortgage
+    #total of all inputs for total monthly expenses
+
+    #define a function for cash flow
+    #total monthly income - total monthly expenses
+    # = total monthly cashflow
+    #total monthly cashflow x 12 = total annual cash flow
+
+    #define a function for cash on cash return
+    #ask for input for down payment
+    #ask for input for closing costs
+    #ask for input for rehab budget
+    #ask for input for misc other
+    #total of all the above to get Total Investment
+
+    #calculate 
+    #annual cash flow / total investment = X
+    #take X * 100 to get cash on cash return!
+
+    #print cash on cash return result
+
+
+
+
