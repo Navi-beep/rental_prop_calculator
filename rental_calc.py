a64a00730a4702854a851b3746614d63188d3381
Navi-Beep <estawars30@gmail.com>
2022-08-05 17:37:03 -0500

    moved stuff around

--- rental_calc.py
+++ rental_calc.py
@@ -5,6 +5,14 @@ class ROI:
     #create initialize function
     def __init__(self, value):
         self.value = value
+
+
+    #define a function for income
+        #ask input for rental income
+        #ask input for landry income
+        #ask input for storage income
+        #ask input for misc income
+    #total all of the income for total monthly income
     
     def income(self):
         ask_1 = int(input("What is your rental income? "))
@@ -14,66 +22,6 @@ class ROI:
 
         total_income = ask_1 + ask_2 + ask_3 + ask_4
         print(f"Total Income : {total_income} ")
-
-    
-    def expenses(self):
-        ask_5 = int(input("What are your tax expenses? "))
-        ask_6 = int(input("What are your insurance expenses? "))
-        ask_7 = int(input("What are your water/sewer expenses? "))
-        ask_8 = int(input("What are your electric/gas expenses? "))
-        ask_9 = int(input("What are your misc expenses? "))
-        
-
-        total_monthly_expenses = ask_5 + ask_6 + ask_7 + ask_8 + ask_9
-        print(f"Total Monthly Expenses: {total_monthly_expenses} ")
-
-    def cash_flow(self):
-
-        total_monthly_cash_flow = ROI()
-
-        total_monthly_cash_flow = total_income - total_monthly_expenses
-
-        total_annual_cash_flow = total_monthly_cash_flow * 12 
-        print(f"Total Annual Cash Flow: {total_annual_cash_flow}") 
-
-
-    def cash_return(self):
-        ask_10 = int(input("What is your down payment?"))
-        ask_11 = int(input("What are your closing costs?"))
-        ask_12 = int(input("What is your rehab budget? "))
-        ask_13 = int(input("What are your misc expenses?"))
-
-        total_investment = ask_10 + ask_11 + ask_12 + ask_13
-
-        cash_on_cash_return = (total_annual_cash_flow / total_investment) * 100
-
-        print(f" Your cash on cash ROI is:  {cash_on_cash_return}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #define a function for income
-        #ask input for rental income
-        #ask input for landry income
-        #ask input for storage income
-        #ask input for misc income
-    #total all of the income for total monthly income
 
     #define a function for expenses
      #ask for input for: taxes
@@ -90,10 +38,33 @@ class ROI:
      #ask for input for: mortgage
     #total of all inputs for total monthly expenses
 
-    #define a function for cash flow
-    #total monthly income - total monthly expenses
-    # = total monthly cashflow
-    #total monthly cashflow x 12 = total annual cash flow
+
+    
+    def expenses(self):
+        ask_5 = int(input("What are your tax expenses? "))
+        ask_6 = int(input("What are your insurance expenses? "))
+        ask_7 = int(input("What are your water/sewer expenses? "))
+        ask_8 = int(input("What are your electric/gas expenses? "))
+        ask_9 = int(input("What are your misc expenses? "))
+        
+
+        total_monthly_expenses = ask_5 + ask_6 + ask_7 + ask_8 + ask_9
+        print(f"Total Monthly Expenses: {total_monthly_expenses} ")
+
+        #define a function for cash flow
+        #total monthly income - total monthly expenses
+        # = total monthly cashflow
+        #total monthly cashflow x 12 = total annual cash flow
+
+    def cash_flow(self):
+
+        total_monthly_cash_flow = ROI()
+
+        total_monthly_cash_flow = total_income - total_monthly_expenses
+
+        total_annual_cash_flow = total_monthly_cash_flow * 12 
+        print(f"Total Annual Cash Flow: {total_annual_cash_flow}") 
+
 
     #define a function for cash on cash return
     #ask for input for down payment
@@ -101,6 +72,19 @@ class ROI:
     #ask for input for rehab budget
     #ask for input for misc other
     #total of all the above to get Total Investment
+
+    def cash_return(self):
+        ask_10 = int(input("What is your down payment?"))
+        ask_11 = int(input("What are your closing costs?"))
+        ask_12 = int(input("What is your rehab budget? "))
+        ask_13 = int(input("What are your misc expenses?"))
+
+        total_investment = ask_10 + ask_11 + ask_12 + ask_13
+
+        cash_on_cash_return = (total_annual_cash_flow / total_investment) * 100
+
+        print(f" Your cash on cash ROI is:  {cash_on_cash_return}")
+
 
     #calculate 
     #annual cash flow / total investment = X
